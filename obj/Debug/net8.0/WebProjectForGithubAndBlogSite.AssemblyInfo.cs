da3525fb550865f4455aff65365f6ced36d30102
Yiğit Çakmakcı <yigitcakmakci117@gmail.com>
2024-09-25 22:06:22 +0300

    HomePage, Portofolio ve Contact sayfasının temelleri atıldı. FrontEnd kısmının ana özellikleri neredeyse tamamlandı. Şimdi ise contact sayfasındaki tasarımı planlayıp siteyi doldurma zamanı!

--- obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305694971e20479ee5e433b95168edb0c12d90ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694bdc7644f84cce095de3da7bb5380aeb0a1461")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
