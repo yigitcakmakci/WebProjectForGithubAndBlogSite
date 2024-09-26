f720462caf2ba493e2af07f0d85f6b6146b8ada7
Yiğit Çakmakcı <yigitcakmakci117@gmail.com>
2024-09-26 20:35:32 +0300

    Bazı renk paletleri değiştirildi ve dive resim eklemek için temeller atıldı. Ancak resim gözükmyüor ve her yenilediğimde sayfa tamamen rastgele şekilde güncel ve eski renkler arasında geçiş yapıyor.

--- obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694bdc7644f84cce095de3da7bb5380aeb0a1461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da3525fb550865f4455aff65365f6ced36d30102")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
