f910d428d4d6f82c35f08696a3a38f2e71414a84
Yiğit Çakmakcı <yigitcakmakci117@gmail.com>
2024-09-26 21:25:44 +0300

    Now, in the update I made, I understood the concepts of Flexbox and grid better. With FlexBox, I learned how to split blocks on the main page. With the grid, I split the other blocks inside the main block. And in this way, a more reliable and more robust site design has emerged. Now I want to add a little more vitality to the site with javascript. (Now I decided to write my articles in English, but I use Translate because my English is not very good. I'm thinking of taking an online English course and learning English soon. I just started working on manufacturing in the factory. Let the rails fall into place, and I will put my life in order.)

--- obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f720462caf2ba493e2af07f0d85f6b6146b8ada7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d790b48f4af7b0e9b90517bccdd09314e1a771c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
