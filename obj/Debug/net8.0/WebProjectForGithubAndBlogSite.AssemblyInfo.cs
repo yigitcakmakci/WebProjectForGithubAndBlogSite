523e0e9f464fe11af505b72ee077a478bd21e516
Yiğit Çakmakcı <yigitcakmakci117@gmail.com>
2024-10-02 22:41:11 +0300

    Improvements were made in the field of design. An easier and more understandable design was aimed (V2) The site is closer to its final form. Now I'm renewing the designs on the portofolio and contact pages. When these are finished, the foundation of the page will be finished. After the site, I hope to turn it into a dynamic site where I can share my personal experiences.

--- obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26036f3830b0c52e2bfddc145df3271c956a1cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2301cb2e40c7541d58eb40e7bb4e18fc4f029858")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
