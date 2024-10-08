8bb88cb528045356cd46d687bbcceb4f237b9386
Yiğit Çakmakcı <yigitcakmakci117@gmail.com>
2024-10-08 20:02:30 +0300

    Yaptığım değişiklikleri hatırlamıyoruım ama bayaa şey değişmiş gibi gözüküyor. Şu andaki durumum sonsuz bir sarhoşluk. İlk başlarda zamanın geçmemesinden rahatsız olduğum fabrikada çalışma olayı artık kafamda yer etmemeye başladı. Alkol tüketmedeğim halde beynim sürekli fena halde uyuşmuş gibi oluyor ve bu aslında zevkli! Bunları yazmamın nedeni bir site oluştururken ki ruh halimi göz önüne almak. Ve gelecekte bunları okurken eğlenmek. Merhaba gelecekteki Yiğit!

--- obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProjectForGithubAndBlogSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2301cb2e40c7541d58eb40e7bb4e18fc4f029858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523e0e9f464fe11af505b72ee077a478bd21e516")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjectForGithubAndBlogSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
